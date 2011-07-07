31b46ebe29b2fd8268ff9acd9959ad9465f2aad8
vrclvrncknm <s.h.sikkema@gmail.com>
2011-07-07 11:44:46 +0000

    default to failOnError: true for saves

---
+++ test/unit/org/dbxp/metabolomicsModule/MetabolomicsModuleTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.dbxp.metabolomicsModule
+
+import grails.test.TagLibUnitTestCase
+
+class MetabolomicsModuleTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
