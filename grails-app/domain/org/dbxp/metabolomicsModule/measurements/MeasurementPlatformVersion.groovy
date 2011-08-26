d4b9c0707b1a38cabc56496eb2496a2875f9dca5
michaelvanvliet <info@michaelvanvliet.nl>
2011-08-26 18:24:41 +0000

    In Code comment changed

--- grails-app/domain/org/dbxp/metabolomicsModule/measurements/MeasurementPlatformVersion.groovy
+++ grails-app/domain/org/dbxp/metabolomicsModule/measurements/MeasurementPlatformVersion.groovy
@@ -10,19 +10,19 @@ class MeasurementPlatformVersion {
 		changeList(nullable: true)
     }
 	
-	/*
-	* returns the object as a HashMap to be used in the API
-	*/
+	/**
+	 * Returns the object as a HashMap to be used in the API
+	 **/
 	HashMap toApi() {
 		return ['measurement_platform': this.measurementPlatform.toApi(), 'version_number': this.versionNumber, 'changelist': this.changeList ?: '']
 	}
 	
-	/*******************************************************************************************************************************************************
-	* Transients
-	*/
+	/**
+	 * Transients
+	 **/
 	static transients = ['features']
 	
 	// fetch features linked to this version
-	List getFeatures(){ return MeasurementPlatformVersionFeature.findAllByMeasurementPlatformVersion(this).collect { it.feature } }
-	//******************************************************************************************************************************************************
+	List getFeatures(){ return MeasurementPlatformVersionFeature.findAllByMeasurementPlatformVersion(this).collect { it.feature } }	
+	
 }
