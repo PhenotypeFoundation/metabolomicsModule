86c35d66251e697baf6743bdda0ea8249567bd75
4np <work@osx.eu>
2011-09-19 11:15:48 +0000

    - upgraded module base

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,12 +1,10 @@
 class UrlMappings {
-
 	static mappings = {
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
-
 		"/"(controller: 'home', action: 'index')
 		"500"(view:'/error')
 	}
