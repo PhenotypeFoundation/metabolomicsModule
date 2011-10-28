01037eb86f8d36f3389575af868ae44707bdceb9
michaelvanvliet <info@michaelvanvliet.nl>
2011-10-28 12:16:20 +0000

    Prototype of FeaturePropertyView(er) Service

---
+++ grails-app/services/org/dbxp/metabolomicsModule/identity/FeaturePropertyService.groovy
@@ -0,0 +1,48 @@
+package org.dbxp.metabolomicsModule.identity
+
+class FeaturePropertyService {
+	
+	def ChemspiderService
+	def ChebiService
+	def LipidmapsService
+	def PubchemService
+
+    static transactional = true
+
+    def view(String label, String value) {
+		
+		//reformat the label to a method compatible format
+		label = label.replaceAll(" ", "").toLowerCase()
+		label = label.replace(label[0], label[0].toUpperCase())
+		
+		try {
+			return this."view${label}"(value)
+		} catch (e) {
+			return value
+		}
+    }
+	
+	def viewInchi(inchi){
+		return "THIS IS AN INCHI ${inchi}"
+	}
+	
+	def viewInchikey(inchikey){
+		return "THIS IS AN INCHIKEY ${inchikey}"
+	}
+	
+	def viewChebi(chebiId){
+		return "THIS IS A CHEBI ID ${chebiId}"
+	}
+	
+	def viewPubchem(pubchemId){
+		return "THIS IS A PUBCHEM ID ${pubchemId}"
+	}
+	
+	def viewLipidmap(LMID){
+		return "THIS IS A Lipidmaps IDtoURL ${LipidmapsService.lipidmapsUrlByLMID(LMID)}"
+	}
+	
+	def viewChemspider(chemspiderId){
+		return "THIS IS A Chemspider IDtoURL ${ChemspiderService.chemspiderUrlByChemspiderId(chemspiderId)}"
+	}
+}
