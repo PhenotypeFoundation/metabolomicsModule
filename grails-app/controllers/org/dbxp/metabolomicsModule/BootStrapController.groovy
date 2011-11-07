0fd9487d3aac18c324efff28d72847550e488279
michaelvanvliet <info@michaelvanvliet.nl>
2011-11-07 12:28:48 +0000

    Bootstrap now compatible with real GSCF instance (default is Assay Y, but has a fall-back to Lipidomics study)

--- grails-app/controllers/org/dbxp/metabolomicsModule/BootStrapController.groovy
+++ grails-app/controllers/org/dbxp/metabolomicsModule/BootStrapController.groovy
@@ -1,6 +1,8 @@
 package org.dbxp.metabolomicsModule
 
 import org.dbxp.metabolomicsModule.measurements.MeasurementPlatform
+import org.dbxp.metabolomicsModule.measurements.MeasurementPlatformVersion
+
 import grails.converters.JSON
 
 /**
@@ -15,40 +17,54 @@ class BootStrapController {
 	
 	def index = {
 		
-		def assayYYY = MetabolomicsAssay.findByName("Assay Y")
+		def assayName 		= "Assay Y"
+		def altAssayName 	= "Lipidomics profile after"
+		def mpName 			= "MP-AAA"
+		def mpvVersion 		= 0.1f
+		
+		def mpv
+		def mp
+		
+		// determine Assay
+		def assay = null
+		if (!MetabolomicsAssay.findByName(assayName)){
+			assay = MetabolomicsAssay.findByName(altAssayName)
+		} else {
+			assay = MetabolomicsAssay.findByName(assayName)
+		}
+		
+		if (assay != null) {
+			
+			log.info "Bootstrapping Assay: ${assay.name} for user: ${session.user}"
+		
+			if (!MeasurementPlatform.findByName(mpName)){
 
-		if (assayYYY) {
-
-			/*
-			 * Import example data for assay aaa
-			 */
-			def file            = new File('testData/assay_yyy_test_data.txt')
-			def uploadedFile    = uploadedFileService.createUploadedFileFromFile(file, session.user)
-				uploadedFile.parse([delimiter: '\t'])
-				uploadedFile.assay = assayYYY
-				uploadedFile.save()
-
-			/*
-			 * this will load example data into:
-			 * - org.dbxp.metabolomicsModule.identity.Feature
-			 * - org.dbxp.metabolomicsModule.measurements.MeasurementPlatform
-			 * - org.dbxp.metabolomicsModule.measurements.MeasurementPlatformVersion
-			 * - org.dbxp.metabolomicsModule.measurements.MeasurementPlatformVersionFeature
-			 */
-
-			def mp = new MeasurementPlatform(name: "MP-AAA").save()
-
-			file = new File('testData/feature_mock_data.xls')
-			def uploadedFeatureFile = uploadedFileService.createUploadedFileFromFile(file, session.user)
-				uploadedFeatureFile.parse()
-
-			assayYYY.measurementPlatformVersion = measurementService.createMeasurementPlatformVersion(uploadedFeatureFile, [versionNumber: 0.1f, measurementPlatform: mp])
-			assayYYY.measurementPlatformVersion.save(failOnError: true)
-			assayYYY.save(failOnError: true)
+				// Upload features file 
+				def uploadedFeatureFile = uploadedFileService.createUploadedFileFromFile(new File('testData/feature_mock_data.xls'), session.user)
+					uploadedFeatureFile.parse()
+				
+				// setup the MeasurementPlatform and Version
+				mp 	= new MeasurementPlatform(name: mpName).save()
+				log.info "Created new MeasurementPlatform ${mpName}"
+				mpv	= measurementService.createMeasurementPlatformVersion(uploadedFeatureFile, [versionNumber: mpvVersion, measurementPlatform: mp])
+				log.info "Created new MeasurementPlatformVersion ${mpvVersion}"
+				
+				// only add data for asssay YYY
+				if (assay.name == assayName){
+					def uploadedFile    = uploadedFileService.createUploadedFileFromFile(new File('testData/assay_yyy_test_data.txt'), session.user)
+					uploadedFile.parse([delimiter: '\t'])
+					uploadedFile.assay = assay
+					uploadedFile.save()
+					log.info "Uploaded example dataset for assay ${assayName}"
+				}
+				
+				assay.measurementPlatformVersion = mpv
+				assay.save(failOnError: true)	
+			} 
+			
+			log.info "Done bootstrapping"
 		}
 
-//		if (request.getHeader('Referer')) redirect(url: request.getHeader('Referer'))
-//		else redirect(controller: 'home')
 		render ([status: 200, message: "loaded mock data"] as JSON)
 	}
 }
