f132ec060e74144a42fb95fc04e31563726120a7
Michael van Vliet <info@michaelvanvliet.nl>
2012-10-02 16:23:20 +0300

    Update src/groovy/org/dbxp/GSCFClient.groovy
    also here replace the symlink with the real code

--- src/groovy/org/dbxp/GSCFClient.groovy
+++ src/groovy/org/dbxp/GSCFClient.groovy
@@ -1,1 +1,21 @@
-../../../../../GSCF-GroovyClient/src/main/groovy/org/dbxp/GSCFClient.groovy
+package org.dbxp
+
+public interface GSCFClient {
+
+  def getStudies()
+	def getSubjectsForStudy(String studyToken)
+	def getAssaysForStudy(String studyToken)
+	def getEventGroupsForStudy(String studyToken)
+	def getEventsForStudy(String studyToken)
+	def getSamplingEventsForStudy(String studyToken)
+	def getSamplesForAssay(String assayToken)
+	def getMeasurementDataForAssay(String assayToken)
+	def getModules()
+	def getEntityTypes()
+	def getTemplatesForEntity(String entityType)
+	def getFieldsForEntity(String entityType, String entityToken)
+	def getFieldsForEntityWithTemplate(String entityType, String templateToken)
+	def createEntity(String entityType, Map fields)
+	def createEntityWithTemplate(String entityType, String templateToken, Map fields)
+
+}
